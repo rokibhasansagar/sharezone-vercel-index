85e5a34e34500c9583ffe48b30fa363c762a539a
Rokib Hasan Sagar <10854164+rokibhasansagar@users.noreply.github.com>
2024-02-14 17:46:22 +0600

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
 
       <main className="flex w-full flex-1 flex-col bg-gray-50 dark:bg-gray-800">
         <Navbar />
-        <div className="mx-auto w-full max-w-5xl py-4 sm:p-4">
+        <div className="mx-auto w-full max-w-7xl py-4 sm:p-4">
           <nav className="mb-4 flex items-center justify-between px-4 sm:px-0 sm:pl-1">
             <Breadcrumb />
             <SwitchLayout />
